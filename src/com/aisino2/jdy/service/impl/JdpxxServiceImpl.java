b5a5b7d2e0429c602007a84bce6532c9d1e207b1
firefoxmmx2 <firefoxmmx@gmail.com>
2012-08-14 09:50:32 +0800

    完善寄递品服务

--- src/com/aisino2/jdy/service/impl/JdpxxServiceImpl.java
+++ src/com/aisino2/jdy/service/impl/JdpxxServiceImpl.java
@@ -3,6 +3,8 @@ package com.aisino2.jdy.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JDialog;
+
 import com.aisino2.core.dao.Page;
 import com.aisino2.jdy.dao.IJdpxxDao;
 import com.aisino2.jdy.domain.Jdpxx;
@@ -17,33 +19,32 @@ public class JdpxxServiceImpl implements IJdpxxService {
 
 
 	public Jdpxx insertJdpxx(Jdpxx jdpxx) {
-		
-		return null;
+		return jdpxxDao.insert(jdpxx);
 	}
 
 	
 	public void updateJdpxx(Jdpxx jdpxx) {
-		// TODO Auto-generated method stub
-
+		if(jdpxx.getId() == null)
+			throw new RuntimeException("需要修改的寄递品ID不能为空");
+		jdpxxDao.update(jdpxx);
 	}
 
 	
 	public void deleteJdpxx(Jdpxx jdpxx) {
-		// TODO Auto-generated method stub
-
+		if(jdpxx.getId() == null )
+			throw new RuntimeException("需要删除的寄递品ID不能为空");
+		jdpxxDao.delete(jdpxx);
 	}
 
 	
 	public List<Jdpxx> findJdpxxs(Jdpxx jdpxx) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdpxxDao.findJdpxxs(jdpxx);
 	}
 
 	
 	public Page findJdpxxsForPage(Map<String, Object> map, int pageno,
 			int pagesize, String dir, String sort) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdpxxDao.findJdpxxsForPage(map, pageno, pagesize, dir, sort);
 	}
 
 }
