5121a38dbb7f40b0b971289f118db94a5ed37a30
Administrator <Administrator@1L3FYPOWOBUACX8>
2012-08-15 08:37:39 +0800

    揽件service更改

--- src/com/aisino2/jdy/service/impl/LjjbxxServiceImpl.java
+++ src/com/aisino2/jdy/service/impl/LjjbxxServiceImpl.java
@@ -8,7 +8,10 @@ import com.aisino2.core.service.BaseService;
 import com.aisino2.jdy.dao.IJdpxxDao;
 import com.aisino2.jdy.dao.IKyjdwpxxDao;
 import com.aisino2.jdy.dao.ILjjbxxDao;
+import com.aisino2.jdy.dao.IRdrjbxxDao;
+import com.aisino2.jdy.domain.Jdpxx;
 import com.aisino2.jdy.domain.Ljjbxx;
+import com.aisino2.jdy.domain.Rdrjbxx;
 import com.aisino2.jdy.service.ILjjbxxService;
 
 public class LjjbxxServiceImpl extends BaseService implements ILjjbxxService{
@@ -16,8 +19,20 @@ public class LjjbxxServiceImpl extends BaseService implements ILjjbxxService{
 	private ILjjbxxDao ljjbxxDao;
 	/**注入揽件物品信息**/
 	private IJdpxxDao jdpxxDao;
+	/**寄递物品信息**/
+	private IRdrjbxxDao rdrjbxxDao;
 	
+	private Ljjbxx setLjjbxx;
 	
+	public Ljjbxx getSetLjjbxx() {
+		return setLjjbxx;
+	}
+	public void setSetLjjbxx(Ljjbxx setLjjbxx) {
+		this.setLjjbxx = setLjjbxx;
+	}
+	public void setRdrjbxxDao(IRdrjbxxDao rdrjbxxDao) {
+		this.rdrjbxxDao = rdrjbxxDao;
+	}
 	public void setJdpxxDao(IJdpxxDao jdpxxDao) {
 		this.jdpxxDao = jdpxxDao;
 	}
@@ -30,11 +45,33 @@ public class LjjbxxServiceImpl extends BaseService implements ILjjbxxService{
 
 	
 	public Ljjbxx insertLjjbxx(Ljjbxx ljjbxx) {
-		// TODO Auto-generated method stub
-		ljjbxx.getLjr();
 		
-		ljjbxxDao.insert(ljjbxx);
-		return null;
+			/***向寄递对象表打入人员信息的数据--得到人员信息ID后再插入到揽件表中去***/
+			//首先判断证件号码，表中是否存在，若不存在则插入
+			Rdrjbxx setRddrjbxx = new Rdrjbxx();
+			setRddrjbxx.setZjhm(ljjbxx.getJjr().getZjhm());
+			setRddrjbxx=rdrjbxxDao.get(setRddrjbxx);
+			if(setRddrjbxx!=null){
+				Rdrjbxx rddrjbxx = new Rdrjbxx();
+				/***向寄递对象--人员信息表插入数据***/
+				rdrjbxxDao.insert(rddrjbxx);
+			}
+			setRddrjbxx.setZjhm(ljjbxx.getSjr().getZjhm());
+			setRddrjbxx=rdrjbxxDao.get(setRddrjbxx);
+			if(setRddrjbxx!=null){
+				Rdrjbxx rddrjbxx = new Rdrjbxx();
+				/***向寄递对象--人员信息表插入数据***/
+				rdrjbxxDao.insert(rddrjbxx);
+			}
+			Ljjbxx setljjbxx = new Ljjbxx();
+			/***向揽件信息表打入揽件数据***/
+			ljjbxxDao.insert(setljjbxx);
+			/***向寄递物品表插入数据***/
+			Jdpxx setjbpxx = new Jdpxx();
+			jdpxxDao.insert(setjbpxx);
+			
+			
+		return setLjjbxx;
 	}
 
 	public void updateLjjbxx(Ljjbxx ljjbxx) {
