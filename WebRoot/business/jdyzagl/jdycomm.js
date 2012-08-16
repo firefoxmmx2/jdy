32e5f40e9be4d6478096e85603dac1a435511381
firefoxmmx2 <firefoxmmx@gmail.com>
2012-08-16 16:14:18 +0800

    添加 公共JS 函数
    /**
     * 获取字典项信息
     * 
     * @param config
     *            {url:'字典后台服务地址', dictcode:'字典代码', value:'真实值'}
     * @return 符合条件的字典项对象[数组]
     */

--- WebRoot/business/jdyzagl/jdycomm.js
+++ WebRoot/business/jdyzagl/jdycomm.js
@@ -32,3 +32,64 @@ function getSubmitParams(expr,params){
 	
 	return params;
 }
+
+/**
+ * 获取字典项信息
+ * 
+ * @param config
+ *            {url:'字典后台服务地址', dictcode:'字典代码', value:'真实值'}
+ * @return 符合条件的字典项对象[数组]
+ */
+function getDictitem(config) {
+	var _config = {
+		url : 'sysadmin/queryChSelDict_dict_item.action',
+		dictcode : '',
+		value : ''
+	};
+	var url_query = "sysadmin/query_dict_item.action";
+	var _ret;
+	var _param = {};
+	// <Params>
+	// <Param name="item_id">31433</Param>
+	// </Params>
+	var dataxml = "<Params>";
+
+	$.extend(_config, config);
+
+	if (!_config.url)
+		throw new Error("参数config {url}信息为空");
+	if (!_config.dictcode)
+		throw new Error("参数config {dictcode}信息为空");
+
+	_param['dict_code'] = _config.dictcode;
+
+	dataxml += '<Param name="dict_code">' + _config.dictcode + '</Param>';
+	if (_config.value) {
+		_param['fact_value'] = _config.value;
+		_param['query_simplepin'] = _config.value;
+		_config.url = url_query;
+		dataxml += '<Param name="fact_value">' + _config.value + '</Param>';
+		dataxml += '<Param name="query_simplepin">' + _config.value
+				+ '</Param>';
+	}
+
+	dataxml += "</Params>";
+	_param['dataxml'] = dataxml;
+	$.ajax({
+				async : false,
+				type : 'post',
+				url : _config.url,
+				data : _param,
+				dataType : 'json',
+				success : function(json) {
+
+					if (json.LDict_item && json.LDict_item.length > 0) {
+						_ret = json.LDict_item;
+					} else if (json.ldata && json.ldata.length > 0) {
+						_ret = json.ldata;
+					}
+
+				}
+			});
+	return _ret;
+}
