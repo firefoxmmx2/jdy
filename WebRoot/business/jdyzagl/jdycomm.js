70768cd23b506de88390fd48688cc2ec31d0d185
Administrator <Administrator@1L3FYPOWOBUACX8>
2012-08-17 09:25:32 +0800

    添加  /*  *将前台ingrid组合成数组的方法  *  */

--- WebRoot/business/jdyzagl/jdycomm.js
+++ WebRoot/business/jdyzagl/jdycomm.js
@@ -32,3 +32,59 @@ function getSubmitParams(expr,params){
 	
 	return params;
 }
+/*
+ *将前台ingrid组合成数组的方法
+ *
+ */
+var degsz = new Array();//第二个数组
+function createszff(childList){
+	
+	if (childList!=null && childList.length>0){
+		var thNameArr = new Array();
+		var listTrId;
+		var isObject = (dataid!=null && typeof(dataid)=="object");
+	    var deg="";
+		for (var i=0;i<childList.length;i++){
+			var oThead=$("#"+childList[i]).find("THEAD");
+			var oTbody=$("#"+childList[i]).find("TBODY");
+			if (oTbody.length>0){
+				oThead.find("TR").find("TH").each(function(indexTH, domEleTH) {
+					var colName = $(domEleTH).attr("name");
+					thNameArr[indexTH] = colName.substr(2);
+				});
+				if(isObject){
+					listTrId = dataid[i];
+				}else{
+					listTrId = dataid;
+				}
+				var decdd=-1;
+				oTbody.find("TR").each(function(indexTR, domEleTR) {
+					decdd++;
+					var dyg="";
+					var dygsz = new Array();//数组
+					var dicdd=-1;
+					$(domEleTR).find("TD").each(function(indexTD, domEleTD) {
+						dicdd++;
+						if(thNameArr[indexTD]!=null&&thNameArr[indexTD]!='null'
+							&&thNameArr[indexTD]!=''&&thNameArr[indexTD]!='undefined'){
+							var colName;
+							if($(domEleTD).find("div").length==0){
+								colName = $(domEleTD).html();
+							}else{
+								colName = $(domEleTD).find("div").html();
+							}
+							//dyg+=colName+"#";
+							dygsz[dicdd]=colName;
+						}
+					});
+					//deg+=dyg+"*";
+					degsz[decdd]=dygsz;
+					//alert(degsz[decdd]);
+				});
+			}
+			
+		}
+		//alert("最后的值="+deg);
+		//alert("最后的数组="+degsz);
+	}
+}
