854dda13fb83e116dec480f8401231e8f3c6aa40
Administrator <Administrator@1L3FYPOWOBUACX8>
2012-08-21 10:03:09 +0800

    揽件action新增方法提交

--- src/com/aisino2/jdy/action/LjxxAction.java
+++ src/com/aisino2/jdy/action/LjxxAction.java
@@ -71,35 +71,6 @@ public class LjxxAction extends PageAction{
 		this.jdpxx = jdpxx;
 	}
 	
-	/***揽件基本信息主页面setable方法***/
-	private void setTableDate_ljjbxx(List lData) {
-		// TODO Auto-generated method stub
-		List lPro = new ArrayList();
-		lPro.add("id");
-		lPro.add("djxh");
-		lPro.add("wldh");
-		lPro.add("jjrxm");
-		lPro.add("zjlx");
-		lPro.add("ljy");
-		lPro.add("ljtbsj");
-		
-		List lCol = new ArrayList();
-		
-		List lDetail = new ArrayList();
-		lDetail.add("setLjxxDetail");
-		lDetail.add("详情");
-		lCol.add(lDetail);
-		List lUpdate = new ArrayList();
-		lUpdate.add("setLjxxUpdate");
-		lUpdate.add("修改");
-		lCol.add(lUpdate);
-
-		
-		Ljjbxx setLjxx = new Ljjbxx();
-		this.setDataCustomer(setLjxx, lData, lPro, null, lCol);
-		this.tabledata = this.getData();
-		totalrows = this.getTotalrows();
-	}
 	
 	/**
      *揽件信息插入action
@@ -118,26 +89,58 @@ public class LjxxAction extends PageAction{
 		return SUCCESS;
     }
 	
+	/**
+	 * 更改揽件信息
+	 * @return
+	 * @throws Exception
+	 */
+	public String update() throws Exception{
+		if(lj == null)
+			throw new RuntimeException("需要修改的派件信息参数不能为空");
+		ljjbxxService.updateLjjbxx(lj);
+		
+		this.result=SUCCESS;
+		return SUCCESS;
+	}
+	
+	/**
+	 * 删除揽件信息
+	 * @return
+	 * @throws Exception
+	 */
+	public String delete() throws Exception{
+		if(lj == null)
+			throw new RuntimeException("需要删除的派件信息参数不能为空");
+		ljjbxxService.deleteLjjbxx(lj);
+		
+		this.result=SUCCESS;
+		return SUCCESS;
+	}
+	/**
+	 * 分页查询揽件信息
+	 * @return
+	 * @throws Exception
+	 */
 	public String queryListlj() throws Exception {
 			
 		Map<String, Object> params = new HashMap<String, Object>();
 		
 		if(lj !=null){
-//			if(lj.getWldh()!=""){//物流单号
-//				params.put("wldh", lj.getWldh());
-//			}
-//			if(lj.getJjr().getXm()!=""){//寄件人姓名
-//				params.put("xm", lj.getJjr().getXm());
-//			}
-//			if(lj.getJjr().getZjlx()!=""){//证件类型
-//				params.put("zjlx", lj.getJjr().getZjlx());
-//			}
-//			if(lj.getJjr().getZjhm()!=""){//证件号码
-//				params.put("zjhm", lj.getJjr().getZjhm());
-//			}
-//			if(lj.getLjr().getCyrybh()!=""){//揽件员
-//				params.put("ljr", lj.getLjr().getCyrybh());
-//			}
+			if(lj.getWldh()!=""){//物流单号
+				params.put("wldh", lj.getWldh());
+			}
+			if(lj.getJjr().getXm()!=""){//寄件人姓名
+				params.put("xm", lj.getJjr().getXm());
+			}
+			if(lj.getJjr().getZjlx()!=""){//证件类型
+				params.put("zjlx", lj.getJjr().getZjlx());
+			}
+			if(lj.getJjr().getZjhm()!=""){//证件号码
+				params.put("zjhm", lj.getJjr().getZjhm());
+			}
+			if(lj.getLjr().getCyrybh()!=""){//揽件员
+				params.put("ljr", lj.getLjr().getCyrybh());
+			}
 		}
 
 		
@@ -165,4 +168,42 @@ public class LjxxAction extends PageAction{
 		this.result = "success";
 		return SUCCESS;
 	}
+	
+	/***揽件基本信息主页面setable方法***/
+	private void setTableDate_ljjbxx(List lData) {
+		// TODO Auto-generated method stub
+		List lPro = new ArrayList();
+		lPro.add("id");
+		lPro.add("djxh");
+		lPro.add("wldh");
+		lPro.add("jjrxm");
+		lPro.add("zjlx");
+		lPro.add("ljy");
+		lPro.add("ljtbsj");
+		
+		List lCol = new ArrayList();
+		
+		List lDetail = new ArrayList();
+		lDetail.add("setLjxxDetail");
+		lDetail.add("详情");
+		lCol.add(lDetail);
+		
+		List lUpdate = new ArrayList();
+		lUpdate.add("setLjxxUpdate");
+		lUpdate.add("修改");
+		lCol.add(lUpdate);
+		
+
+		List lDelete=new ArrayList();
+		lDelete.add("setLjxxDelete");
+		lDelete.add("删除");
+		lCol.add(lDelete);
+
+		
+		Ljjbxx setLjxx = new Ljjbxx();
+		this.setDataCustomer(setLjxx, lData, lPro, null, lCol);
+		this.tabledata = this.getData();
+		totalrows = this.getTotalrows();
+	}
+	
 }
