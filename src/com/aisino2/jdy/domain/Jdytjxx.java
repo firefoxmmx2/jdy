68fd480b72f2532121da8f6de733c1f4c8c64439
firefoxmmx2 <firefoxmmx@gmail.com>
2012-09-24 21:07:05 +0800

    为新增查询扩充字段信息。

--- src/com/aisino2/jdy/domain/Jdytjxx.java
+++ src/com/aisino2/jdy/domain/Jdytjxx.java
@@ -25,7 +25,101 @@ public class Jdytjxx implements Serializable {
 	private String gxdwbm;
 	private String gxdwmc;
 	
+	//运行情况
+	private String qyzs;
+	private String zjs;
+	private String cyrys;
+	private String ljs;
+	private String pjs;
+	private String wscqys;
 	
+	private Integer zrs;
+	private String yyztmc;
+	private String zjztmc;
+	private String zt;
+	private String jjlxmc;
+	private String qybm;
+	
+	private Integer departlevel;
+	
+	public Integer getDepartlevel() {
+		return departlevel;
+	}
+	public void setDepartlevel(Integer departlevel) {
+		this.departlevel = departlevel;
+	}
+	public String getQybm() {
+		return qybm;
+	}
+	public void setQybm(String qybm) {
+		this.qybm = qybm;
+	}
+	public String getYyztmc() {
+		return yyztmc;
+	}
+	public void setYyztmc(String yyztmc) {
+		this.yyztmc = yyztmc;
+	}
+	public String getZjztmc() {
+		return zjztmc;
+	}
+	public void setZjztmc(String zjztmc) {
+		this.zjztmc = zjztmc;
+	}
+	public String getJjlxmc() {
+		return jjlxmc;
+	}
+	public void setJjlxmc(String jjlxmc) {
+		this.jjlxmc = jjlxmc;
+	}
+	public Integer getZrs() {
+		return zrs;
+	}
+	public void setZrs(Integer zrs) {
+		this.zrs = zrs;
+	}
+	public String getZt() {
+		return zt;
+	}
+	public void setZt(String zt) {
+		this.zt = zt;
+	}
+	public String getQyzs() {
+		return qyzs;
+	}
+	public void setQyzs(String qyzs) {
+		this.qyzs = qyzs;
+	}
+	public String getZjs() {
+		return zjs;
+	}
+	public void setZjs(String zjs) {
+		this.zjs = zjs;
+	}
+	public String getCyrys() {
+		return cyrys;
+	}
+	public void setCyrys(String cyrys) {
+		this.cyrys = cyrys;
+	}
+	public String getLjs() {
+		return ljs;
+	}
+	public void setLjs(String ljs) {
+		this.ljs = ljs;
+	}
+	public String getPjs() {
+		return pjs;
+	}
+	public void setPjs(String pjs) {
+		this.pjs = pjs;
+	}
+	public String getWscqys() {
+		return wscqys;
+	}
+	public void setWscqys(String wscqys) {
+		this.wscqys = wscqys;
+	}
 	public Integer getSeqnum() {
 		return seqnum;
 	}
